75b6e3ad772a4f7a9ec07963b12aa6189cdacce5
Hossam Ibrahim <hossamibrahim.fcai23@gmail.com>
2026-09-10 19:19:18 +0300

    handle authentication (token check) and role-based authorization (User , Admin)

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -10,25 +10,39 @@ import ResturantPage from "../Pages/Restaurant/RestaurantPage";
 import AuthenticationLayout from "../Pages/Authentication/AuthenticationLayout";
 import Login from "../Pages/Authentication/Login";
 import SignUp from "../Pages/Authentication/SignUp";
+import ProtectedRoute from "./ProtectedRoute";
+import ProtectedAuth from "./ProtectedAuth";
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-
-      <Route path="/landingPage" element={<LandingPage />} />
-      <Route path="/Authentication" element={<AuthenticationLayout />}>
-        <Route index element={<Navigate to="Login" replace />} />
-        <Route path="Login" element={<Login />} />
-        <Route path="Sign_Up" element={<SignUp />} />
+      {/*if there are authenticated user or admin can not return to landing page without logout first so empty */}
+      <Route element={<ProtectedAuth />}>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/landingPage" element={<LandingPage />} />
       </Route>
-      <Route path="/Dashboard" element={<DashboardLayout />}>
-        <Route index element={<Navigate to="Statistics" replace />} />
-        <Route path="Statistics" element={<Statistics />} />
-        <Route path="Menu" element={<Menu />} />
-        <Route path="Orders" element={<Orders />} />
-        <Route path="Users" element={<Users />} />
+      {/*if there are authenticated user or admin can not return to Authentication pages without logout first so empty */}
+      <Route element={<ProtectedAuth />}>
+        <Route path="/Authentication" element={<AuthenticationLayout />}>
+          <Route index element={<Navigate to="Login" replace />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Sign_Up" element={<SignUp />} />
+        </Route>
       </Route>
-      <Route path="/Resturant" element={<ResturantPage />} />
+      {/*only admin has access to this route */}
+      <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
+        <Route path="/Dashboard" element={<DashboardLayout />}>
+          <Route index element={<Navigate to="Statistics" replace />} />
+          <Route path="Statistics" element={<Statistics />} />
+          <Route path="Menu" element={<Menu />} />
+          <Route path="Orders" element={<Orders />} />
+          <Route path="Users" element={<Users />} />
+        </Route>
+      </Route>
+      {/* admin and user have access to this route */}
+      <Route element={<ProtectedRoute allowedRoles={["admin", "user"]} />}>
+        <Route path="/Resturant" element={<ResturantPage />} />
+      </Route>
+      {/* Unkown Path */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
---
+++ src/router/ProtectedAuth.jsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from "react-router-dom";
+
+export default function ProtectedAuth() {
+  const token = localStorage.getItem("token");
+  const role = localStorage.getItem("role");
+
+  if (token) {
+    if (role == "admin") {
+      return <Navigate to="/Dashboard" replace />;
+    }
+    return <Navigate to="/Resturant" replace />;
+  }
+  return <Outlet />;
+}
